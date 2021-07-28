32623b5b046e04e58218abb2e7c1601028827071
malavjoshi23 <20it046@charusat.edu.in>
2021-07-28 14:22:55 +0530

    prac1

---
+++ Myfirstproject/src/demo1.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+import java.util.*;
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner sc= new Scanner(System.in);
+        String s1= sc.nextLine();
+        System.out.println(s1);
+    }
+}
